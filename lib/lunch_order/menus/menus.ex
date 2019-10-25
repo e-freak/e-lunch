24fe51c3526696e211fa29a76296afccf09b6119
okamoto <takuya.okamoto@jrits.ricoh.co.jp>
2019-10-25 09:39:34 +0900

    menu price update

--- lib/lunch_order/menus/menus.ex
+++ lib/lunch_order/menus/menus.ex
@@ -21,13 +21,13 @@ defmodule LunchOrder.Menus do
   """
   def list_menus do
     [
-      %Menu{id: 1, name: "普通弁当", price: 300, symbol: "○"},
-      %Menu{id: 2, name: "普通弁当 ご飯大盛", price: 320, symbol: "大"},
-      %Menu{id: 3, name: "普通弁当 ご飯小盛", price: 280, symbol: "小"},
-      %Menu{id: 4, name: "おかずのみ", price: 250, symbol: "お"},
+      %Menu{id: 1, name: "普通弁当", price: 350, symbol: "○"},
+      %Menu{id: 2, name: "普通弁当 ご飯大盛", price: 370, symbol: "大"},
+      %Menu{id: 3, name: "普通弁当 ご飯小盛", price: 330, symbol: "小"},
+      %Menu{id: 4, name: "おかずのみ", price: 300, symbol: "お"},
       %Menu{id: 5, name: "ご飯のみ", price: 150, symbol: "ご"},
-      %Menu{id: 6, name: "牛丼", price: 350, symbol: "牛"},
-      %Menu{id: 7, name: "カレーライス", price: 350, symbol: "カ"}
+      %Menu{id: 6, name: "牛丼", price: 400, symbol: "牛"},
+      %Menu{id: 7, name: "カレーライス", price: 400, symbol: "カ"}
     ]
   end
 
