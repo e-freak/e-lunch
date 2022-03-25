dc0944838d46feb9f92b9496fdfd4f81e7bceffd
takuya-okamoto-rits <39112467+takuya-okamoto-rits@users.noreply.github.com>
2022-03-25 12:59:52 +0900

    update price

--- lib/lunch_order/menus/menus.ex
+++ lib/lunch_order/menus/menus.ex
@@ -24,7 +24,7 @@ defmodule LunchOrder.Menus do
       %Menu{id: 1, name: "普通弁当", price: 400, symbol: "○"},
       %Menu{id: 2, name: "普通弁当 ご飯大盛", price: 420, symbol: "大"},
       %Menu{id: 3, name: "普通弁当 ご飯小盛", price: 380, symbol: "小"},
-      %Menu{id: 4, name: "おかずのみ", price: 300, symbol: "お"},
+      %Menu{id: 4, name: "おかずのみ", price: 350, symbol: "お"},
       %Menu{id: 5, name: "ご飯のみ", price: 200, symbol: "ご"},
       %Menu{id: 6, name: "牛丼", price: 400, symbol: "牛"},
       %Menu{id: 7, name: "カレーライス", price: 400, symbol: "カ"}
